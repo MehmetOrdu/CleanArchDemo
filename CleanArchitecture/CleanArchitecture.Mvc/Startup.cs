ea6db8bba7afd655c74a456d24f6cead569498c5
MehmetOrdu <mehmt.ordu@gmail.com>
2019-09-28 09:45:23 +0300

    Initial Commit of Infrastructure.Data Project Update  MVC for new UniversityDBContext

--- CleanArchitecture/CleanArchitecture.Mvc/Startup.cs
+++ CleanArchitecture/CleanArchitecture.Mvc/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.EntityFrameworkCore;
 using CleanArchitecture.Mvc.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using CleanArchitecture.Infrastructure.Data.Context;
 
 namespace CleanArchitecture.Mvc
 {
@@ -41,7 +42,10 @@ namespace CleanArchitecture.Mvc
             services.AddDefaultIdentity<IdentityUser>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-
+            services.AddDbContext<UniversityDBContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("UniversityDBConnection"));
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
